5523d8d78afe3851e2fbf2b43e6410ab220a003b
Satya Pamarty <s_pamarty@yahoo.com>
2024-10-07 19:29:37 +0530

    Updated app.py after fixing inline images

--- app.py
+++ app.py
@@ -175,6 +175,9 @@ def update_epub_descriptions(epub_path, new_descriptions):
                             
                             return full_match
 
+                        # Update content for all img tags
+                        content_str = re.sub(r'<img[^>]+>', lambda m: update_content(m.group(0)), content_str)
+
                         # Update content within figure tags containing images
                         content_str = re.sub(r'<figure[^>]*>.*?</figure>', update_content, content_str, flags=re.DOTALL)
 
